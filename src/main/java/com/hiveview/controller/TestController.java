b7e273285ce2f8239a09eb56721a88a659f6210b
xiaomeng <Anniemqy@163.com>
2019-05-24 18:32:52 +0800

    test

--- src/main/java/com/hiveview/controller/TestController.java
+++ src/main/java/com/hiveview/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.hiveview.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sun.awt.image.ImageWatched;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.SQLOutput;
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,13 +38,150 @@ public class TestController {
 //        System.out.println(res);
 
         // 有效的括号
-//        Integer res = TestController.validBrackets("MCMXCIV");
+
+        // 有效的括号
+//        Boolean res = TestController.validBrackets("()");
 //        System.out.println(res);
+
+        // 有效的括号
+
+        // 合并两个有序链表 todo
+//        LinkedList<Integer> list1 = new LinkedList<Integer>();
+//        list1.add(1);
+//        list1.add(2);
+//        list1.add(4);
+//        LinkedList<Integer> list2 = new LinkedList<Integer>();
+//        list2.add(1);
+//        list2.add(3);
+//        list2.add(4);
+//        System.out.println(list1);
+//        System.out.println(list2);
+//        System.exit(0);
+//        LinkedList res = TestController.mergeTwoLists(list1, list2);
+
+        // 删除排序数组中的重复项
+//        int[] nums = {0, 0, 1, 2, 2, 3, 3, 4, 4, 5, 5, 7, 7};
+//        Integer res = TestController.removeRepeat(nums);
+//        System.out.println(res);
+
+        // 最长公共前缀
+//        String[] strs = {"flower","flow","flight"};
+//        String result = TestController.longestPublicPrefix(strs);
+//        System.out.println(result);
+
+        // 实现strStr，第二个字符串在第一个字符串中首次出现的位置
+//        Integer result = TestController.strStr("abcbcd", "bcd");
+//        System.out.println(result);
+
+        // 实现strStr，第二个字符串在第一个字符串中首次出现的位置
+        String result = TestController.countAndSay(8);
+        System.out.println(result);
     }
 
-//    public static int validBrackets(String str){
-//
+    public static String countAndSay(int n) {
+        String str = "1";
+        for (int i = 2; i <= n; i++) {
+            StringBuilder builder = new StringBuilder();
+            char pre = str.charAt(0);
+            int count = 1;
+            for (int j = 1; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (c == pre) {
+                    count++;
+                } else {
+                    builder.append(count).append(pre);
+                    pre = c;
+                    count = 1;
+                }
+            }
+            builder.append(count).append(pre);
+            str = builder.toString();
+        }
+
+        return str;
+    }
+
+    // 实现strStr，第二个字符串在第一个字符串中首次出现的位置
+    public static Integer strStr(String str1, String str2){
+        int str1Len = str1.length();
+        int str2Len = str2.length();
+        if(str1Len < str2Len) return -1;
+        if(str2Len == 0) return 0;
+        for (int i = 0; i < str1Len - str2Len + 1; i++) {
+            if(str1.substring(i, i+str2Len).equals(str2)) return i;
+        }
+        return -1;
+    }
+
+    // 最长公共前缀
+    public static String longestPublicPrefix(String[] strs){
+        int len = strs.length;
+        if(strs == null || len == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < len; i++) {
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    // 删除排序数组中的重复项
+    public static int removeRepeat(int[] nums){
+        int len = nums.length;
+        if(len == 0) return 0;
+        int num = 0;
+        for (int j = 1; j < len; j++) {
+            if(nums[j] != nums[j-1]){
+                num++;
+            }
+        }
+        return num + 1;
+    }
+
+//    public static LinkedList mergeTwoLists(LinkedList l1, LinkedList l2) {
+//        ListNode dummy = new ListNode(0);
+//        ListNode p = dummy;
+//        while (l1 != null && l2 != null){
+//            if (l1.val < l2.val){
+//                p.next = l1;
+//                l1 = l1.next;
+//            }
+//            else {
+//                p.next = l2;
+//                l2 = l2.next;
+//            }
+//            p = p.next;
+//        }
+//        if (l1 == null) p.next = l2;
+//        if (l2 == null) p.next = l1;
+//        return dummy.next;
 //    }
+
+    // 有效的括号
+    public static Boolean validBrackets(String str){
+        Map<Character, Character> map = new HashMap<>();
+        map.put('}', '{');
+        map.put(']', '[');
+        map.put(')', '(');
+
+        Stack<Character> stack = new Stack<Character>();
+
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            char c = str.charAt(i);
+            char topElement = stack.empty() ? '#' : stack.pop();
+            if (map.containsKey(c)) {
+                if(map.get(c) != topElement){
+                    return false;
+                }
+            }else{
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
 
     // 罗马数字转整数
     public static int luomaToNum(String str){
